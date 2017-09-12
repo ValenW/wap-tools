4582cdf0d15b838478da78586ecc4781ede8eb21
zuo_ji <zuo_ji@worksap.co.jp>
2017-09-12 16:08:32 +0800

    refact project

---
+++ src/main/java/com/wap/tools/Application.java
@@ -0,0 +1,43 @@
+package com.wap.tools;
+
+import com.wap.tools.service.TextResourceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by zuo_ji on 17-9-8.
+ */
+public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    private TextResourceService textResourceService;
+
+    public Application(){
+        this.textResourceService=new TextResourceService();
+    }
+
+    public void genJson() {
+        String csvPath = System.getProperty("user.dir") + "/csv/output.csv";
+        String jsonPath = "json/";
+        textResourceService.genTextJson(csvPath, jsonPath);
+    }
+
+    public void readFromJson() {
+//        String path = "/home/zuo_ji/HUE/WorkSpace/Develop/hue-hr-recruiting/hue-hr-recruiting-version-up/src/main/resources/cassandra/data/TextDefDto";
+        String path = "/home/zuo_ji/Desktop/json/";
+        try {
+            textResourceService.read(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+//        new Application().genJson();
+        new Application().readFromJson();
+
+    }
+}
+
--- src/main/java/com/wap/tools/service/TextResourceService.java
+++ src/main/java/com/wap/tools/service/TextResourceService.java
@@ -1,6 +1,6 @@
-package com.wap.tools;
+package com.wap.tools.service;
 
-
+import com.wap.tools.dao.CSVReader;
 import com.wap.tools.dto.TextCsvDto;
 import com.wap.tools.dto.TextDefDto;
 import jodd.io.FileUtil;
@@ -8,22 +8,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.json.JsonParser;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
- * Created by zuo_ji on 17-9-11.
+ * Created by zuo_ji on 17-9-12.
  */
-public class ReadTextResource {
-    private static Logger log = LoggerFactory.getLogger(ReadTextResource.class);
+@Service
+public class TextResourceService {
+    private Logger log= LoggerFactory.getLogger(this.getClass());
 
-
-
+    public void genTextJson(String csvPath,String jsonPath){
+        List<TextCsvDto> textDefDtos = CSVReader.loadObjectList(TextCsvDto.class,csvPath);
+        List<TextDefDto> dtos=textDefDtos.stream()
+                .map(it->it.toTextDefDtos())
+                .flatMap(it-> it.stream())
+                .collect(Collectors.toList());
+        String json = getTextDefJson(dtos);
+        try {
+            String fileName= LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))+"_INSERT_com.worksap.company.hue.core.dto.TextDefDto.json";
+            FileUtil.writeString(jsonPath+"/"+fileName, json);
+            log.info("success write content to json file {}", jsonPath+fileName);
+        } catch (IOException e) {
+            log.error("write json to file error {}",e);
+            throw new RuntimeException("写入文件时错误");
+        }
+    }
 
 
 
@@ -39,7 +57,7 @@ public class ReadTextResource {
             filePaths.add(f.getAbsolutePath());
         }
 
-        filePaths.sort((a,b)->b.compareTo(a));
+        filePaths.sort(Comparator.reverseOrder());
 
         for (String filePath: filePaths) {
             log.info("start to read {}",filePath);
@@ -56,7 +74,11 @@ public class ReadTextResource {
         writeToCsv(fromTextDefSet(textDefDtos));
     }
 
-    public void writeToCsv(List<TextCsvDto> dtos) {
+    /**
+     * write to csv file.
+     * @param dtos
+     */
+    private void writeToCsv(List<TextCsvDto> dtos) {
         StringBuilder stringBuilder = new StringBuilder("textId,en,ja\n");
         for (TextCsvDto text : dtos) {
             stringBuilder.append('"').append(text.getTextId()).append('"').append(",")
@@ -65,6 +87,7 @@ public class ReadTextResource {
         }
         try {
             FileUtil.writeString("csv/output.csv",stringBuilder.toString());
+            log.info("write to csv file:{}","csv/output.csv");
         } catch (IOException e) {
             e.printStackTrace();
             log.error("write to file,",e);
@@ -72,7 +95,7 @@ public class ReadTextResource {
 
     }
 
-    public List<TextCsvDto> fromTextDefSet(Set<TextDefDto> set) {
+    private List<TextCsvDto> fromTextDefSet(Set<TextDefDto> set) {
         Map<String, TextCsvDto> map = new HashMap<>();
         for (TextDefDto dto : set) {
             if (map.get(dto.getTextId()) == null) {
@@ -100,17 +123,17 @@ public class ReadTextResource {
 
 
     /**
-     * sample:  {"key":{"class":"com.worksap.company.framework.textresource.TextResourceKey","pl":"{\"textId\":\"recruiting.applicant_communication.mail_sending.recommended_reason_often_used\",\"locale\":\"ja\",\"plural\":\"\"}"},"value":"よく使用される","systemFlg":false}
+     * sample json:  {"key":{"class":"com.worksap.company.framework.textresource.TextResourceKey","pl":"{\"textId\":\"recruiting.applicant_communication.mail_sending.recommended_reason_often_used\",\"locale\":\"ja\",\"plural\":\"\"}"},"value":"よく使用される","systemFlg":false}
      *
      * @param json
      */
-    public TextDefDto readjson(String json) {
+    private TextDefDto readjson(String json) {
         TextDefDto textDefDto = new TextDefDto();
         String regex = "textId\\\\\\\":\\\\\\\"(.*?)\\\\\\\",\\\\\\\"locale\\\\\\\":\\\\\\\"(.*)\\\\\\\",\\\\\\\"plural.*value\\\":\\\"(.*)\\\",\\\"systemFlg";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(json);
         while (matcher.find()) {
-         //   log.info("textid:{},locale:{},value:{}", matcher.group(1), matcher.group(2), matcher.group(3));
+            //   log.info("textid:{},locale:{},value:{}", matcher.group(1), matcher.group(2), matcher.group(3));
             String value=matcher.group(3);
             //字段中的双引号用连续2个双引号表示。同时，该字段必须用双引号包住。
             value=value.replace("\\\"","\"\"");
@@ -119,12 +142,31 @@ public class ReadTextResource {
         return textDefDto;
     }
 
-    public static void main(String[] args) {
-        String path = "/home/zuo_ji/HUE/WorkSpace/Develop/hue-hr-recruiting/hue-hr-recruiting-version-up/src/main/resources/cassandra/data/TextDefDto";
-        try {
-            new ReadTextResource().read(path);
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * format to json string
+     * @param textDefDtos
+     * @return json string
+     */
+    private  String getTextDefJson(List<TextDefDto> textDefDtos) {
+        final String template = "{\"class\":\"com.worksap.company.hue.core.dto.TextDefDto\",\"pl\":\"{\\\"key\\\":" +
+                "{\\\"class\\\":\\\"com.worksap.company.framework.textresource.TextResourceKey\\\",\\\"pl\\\":" +
+                "\\\"{\\\\\\\"textId\\\\\\\":\\\\\\\"%s\\\\\\\"," +
+                "\\\\\\\"locale\\\\\\\":\\\\\\\"%s\\\\\\\",\\\\\\\"plural\\\\\\\":\\\\\\\"\\\\\\\"}\\\"},\\\"value\\\":" +
+                "\\\"%s\\\",\\\"systemFlg\\\":false}\"}";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < textDefDtos.size(); i++) {
+            TextDefDto item = textDefDtos.get(i);
+            item.setValue(item.getValue().replace("\"","\\\\\\\""));
+            String text = String.format(template, item.getTextId(), item.getLanguage(), item.getValue());
+            stringBuilder.append(text);
+            if (i != textDefDtos.size() - 1) {
+                stringBuilder.append(",\n");
+            }
         }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
+
+
 }
